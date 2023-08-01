06ef3b201923ee9898191cc16e6caee1f7359bfb
Junhong Liu <15351448567@163.com>
2023-08-01 15:31:48 +0800

    home-人气推荐业务实现

--- src/views/Home/components/HomeNew.vue
+++ src/views/Home/components/HomeNew.vue
@@ -1,7 +1,7 @@
 <script setup>
 import HomePanel from './HomePanel.vue'
 import { findNewAPI } from '@/apis/home'
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
 
 // 获取数据
 const newList = ref([])
@@ -11,9 +11,7 @@ const getNewList = async () => {
   newList.value = res.result
 }
 
-onMounted(() => {
-  getNewList()
-})
+getNewList()
 </script>
 
 <template>
