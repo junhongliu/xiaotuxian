8e13e3a8e1121299572498f894c783987c70079f
Junhong Liu <15351448567@163.com>
2023-08-03 19:36:49 +0800

    401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,8 +1,9 @@
 // axios基础的封装
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
-import { useUserStore } from '@/stores/user'
 import 'element-plus/theme-chalk/el-message.css'
+import { useUserStore } from '@/stores/user'
+import router from '@/router'
 
 const httpInstance = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -29,11 +30,19 @@ httpInstance.interceptors.request.use(
 httpInstance.interceptors.response.use(
   res => res.data,
   e => {
+    const userStore = useUserStore()
     // 统一错误提示
     ElMessage({
       type: 'warning',
       message: e.response.data.message
     })
+    // 401 token失效处理
+    // 1. 清楚本地用户数据
+    // 2. 跳转到登录页
+    if (e.response.status === 401) {
+      userStore.clearUserInfo()
+      router.push('/login')
+    }
     return Promise.reject(e)
   }
 )
