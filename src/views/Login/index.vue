d1342eedbe4391d1d395698aac1920442a44ce8a
Junhong Liu <15351448567@163.com>
2023-08-03 17:32:50 +0800

    登录-pinia管理用户数据

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -2,11 +2,14 @@
 // 表单校验 （账号名 + 密码）
 
 import { ref } from 'vue'
-import { loginAPI } from '@/apis/user'
 
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import { useRouter } from 'vue-router'
+
+import { useUserStore } from '@/stores/user.js'
+
+const userStore = useUserStore()
 
 // 1. 准备表单对象
 const form = ref({
@@ -52,8 +55,7 @@ const doLogin = () => {
     // 以valid作为判断条件 如果通过校验才执行登录逻辑
     if (valid) {
       // TODO LOGIN
-      const res = await loginAPI({ account, password })
-      console.log(res)
+      await userStore.getUserInfo({ account, password })
       // 1. 提示用户
       ElMessage({ type: 'success', message: '登录成功' })
       // 2. 跳转首页
